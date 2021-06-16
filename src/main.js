f85b1f310b5fc958b27cdd78aede22e79bf3cd10
bcashmoney <bryan@tangibleplay.com>
2021-06-16 16:09:44 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+
+    miro.onReady(() => {
+      miro.initialize({
+        extensionPoints: {
+          bottomBar: {
+            title: 'Some title',
+            svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+            onClick: () => {
+              alert('Hi!')
+
+
+
+            } 
+          }
+        }
+      })
+    })
+
+/*
 import Tabletop from 'tabletop'
 
 const ROW_HEIGHT = 30
@@ -84,3 +103,4 @@ async function syncWithSheet() {
     }
   })
 }
+*/
