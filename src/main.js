fd47548dbeb438b88384b8d0e33d555e77fdc985
bcashmoney <bryan@tangibleplay.com>
2021-06-16 16:14:26 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const SPREADSHEET_URL =
+  'https://docs.google.com/spreadsheets/d/121-56BwZe8Cws0A8xE_cSGXc64YD_bBPfQM8o2YVnaM/edit?usp=sharing'
 
     miro.onReady(() => {
       miro.initialize({
@@ -5,13 +7,12 @@
           bottomBar: {
             title: 'Some title',
             svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
-            onClick: () => {
-              alert('Hi!')
-
-
-
-            } 
+            onClick: syncWithSheet
           }
         }
       })
     })
+
+async function syncWithSheet() {
+    alert('Hi!')
+}
