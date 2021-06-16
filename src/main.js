6120639bb6d3e15aa7aea0a5c6a0a1dbc95d84c5
bcashmoney <bryan@tangibleplay.com>
2021-06-16 16:16:10 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,12 @@ const SPREADSHEET_URL =
           bottomBar: {
             title: 'Some title',
             svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
-            onClick: syncWithSheet
+            oonClick: () => {
+              alert('Hi!!!!!!')
+
+
+
+            } 
           }
         }
       })
